9aa1f8ad91f6f048dee8ead160c00d7b8a9b5b78
Simon PERTUS <simon.pertus@gmail.com>
2024-08-27 10:16:16 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'keep-bye-skip',
+  webDir: 'www'
+};
+
+export default config;
