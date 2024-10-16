feface2ab32b9b84a6a359a7b9d36252a7922063
Simon PERTUS <pertus.s@mipih.fr>
2024-10-16 16:32:53 +0200

    commencement travail twists

--- src/app/services/twist.service.ts
+++ src/app/services/twist.service.ts
@@ -6,108 +6,151 @@ import { Injectable } from '@angular/core';
 export class TwistService {
   private twists = [
     {
+      id: 'sans-un-bruit',
       title: 'Sans un bruit',
       description: 'Le provocateur n\'a plus le droit de parler jusqu\'à la fin du duel.',
       emoji: '🤐',
-      card:true,
-      revealTwist:false,
+      card: true,
+      revealTwist: false, // Caché
     },
     {
+      id: 'ange-gardien',
       title: 'Ange gardien',
       description: 'Si la cible prend le téléphone, les deux duellistes sont sauvés. Sinon, les deux sont éliminés.',
       emoji: '😇',
-      card:false,
-      revealTwist:false,
+      card: false,
+      revealTwist: false, // Caché
     },
     {
+      id: 'kamikaze',
       title: 'Kamikaze',
       description: 'Si la cible prend le téléphone, les deux duellistes sont éliminés. Sinon, ils sont sauvés.',
       emoji: '💣',
-      card:false,
-      revealTwist:false,
+      card: false,
+      revealTwist: false, // Caché
     },
     {
+      id: 'democratie',
       title: 'Démocratie',
       description: 'Tous les joueurs (sauf les duellistes) votent pour garder l’un des deux duellistes.',
       emoji: '🗳️',
-      card:false,
-      revealTwist:true,
+      card: false,
+      revealTwist: true, // Obligatoire
     },
     {
+      id: 'une-vie-de-plus',
       title: 'Une vie de plus',
       description: 'Le gagnant du duel gagne une vie supplémentaire.',
       emoji: '💖',
-      card:true,
-      revealTwist:false,
+      card: true,
+      revealTwist: false, // Caché
     },
     {
+      id: 'mort-vivant',
       title: 'Mort vivant',
       description: 'Le provocateur joue contre le premier joueur éliminé sur sa gauche. Si le joueur mort gagne, il revient à la vie.',
       emoji: '🧟',
-      card:true,
-      revealTwist:true,
+      card: true,
+      revealTwist: true, // Obligatoire
     },
     {
+      id: 'la-verite-si-je-mens',
       title: 'La vérité si je mens',
       description: 'Si le provocateur ment une seule fois, il est éliminé.',
       emoji: '🤥',
-      card:true,
-      revealTwist:false,
+      card: true,
+      revealTwist: false, // Caché
     },
     {
+      id: 'esquive',
       title: 'L\'esquive',
-      description: 'Le joueur éliminé esquive la mort si un joueur se trouve à la gauche de la cible.',
+      description: 'Le joueur éliminé esquive la mort si un joueur se trouve directement à la gauche de la cible.',
       emoji: '🏃‍♂️',
-      card:true,
-      revealTwist:false,
+      card: true,
+      revealTwist: false, // Caché
     },
     {
+      id: 'roulette-russe',
       title: 'Roulette russe',
-      description: 'Le provocateur et la cible tirent à tour de rôle. Celui qui tire "Bye" en premier est éliminé.',
+      description: 'Le provocateur et la cible appuient sur la détente à tour de rôle. Celui qui tire une balle est éliminé.',
       emoji: '🎲',
-      card:false,
-      revealTwist:true,
+      card: false,
+      revealTwist: true, // Obligatoire
     },
     {
+      id: 'remplacant',
       title: 'Le remplaçant',
       description: 'Le provocateur choisit un autre joueur pour le remplacer dans le duel.',
       emoji: '🫵',
-      card:true,
-      revealTwist:true,
+      card: true,
+      revealTwist: true, // Obligatoire
     },
     {
+      id: 'ennemi',
       title: 'L\'ennemi',
       description: 'Le provocateur choisit un autre joueur pour devenir la cible.',
       emoji: '🎯',
-      card:true,
-      revealTwist:true,
+      card: true,
+      revealTwist: true, // Obligatoire
     },
     {
+      id: 'la-paix',
       title: 'La paix',
       description: 'Le provocateur peut choisir d\'annuler le duel.',
       emoji: '✌️',
-      card:true,
-      revealTwist:true,
+      card: true,
+      revealTwist: true, // Obligatoire
     },
     {
+      id: 'conseiller',
       title: 'Le conseiller',
-      description: 'La cible choisit un joueur qui décidera si elle doit prendre le téléphone ou non.',
+      description: 'La cible choisit un joueur qui décidera à sa place si elle doit prendre le téléphone ou non.',
       emoji: '🗣️',
-      card:true,
-      revealTwist:true,
+      card: true,
+      revealTwist: true, // Obligatoire
     },
     {
+      id: 'inversion',
       title: 'Inversion',
       description: 'La cible devient provocateur et reçoit une nouvelle carte.',
       emoji: '🔄',
-      card:false,
-      revealTwist:true,
+      card: false,
+      revealTwist: true, // Obligatoire
     },
+    {
+      id: 'compte-a-rebours',
+      title: 'Compte à rebours',
+      description: 'La cible dispose de 30 secondes pour décider de prendre ou non le téléphone. Si elle n’agit pas dans ce délai, la carte est révélée et la décision est prise.',
+      emoji: '⏳',
+      card: true,
+      revealTwist: true, // Obligatoire
+    },
+    {
+      id: 'lies-jusqua-la-mort',
+      title: 'Liés jusqu\'à la mort',
+      description: 'Le duel est annulé et le provocateur et la cible sont liés au même destin jusqu\'à la fin de la partie.',
+      emoji: '🤝',
+      card: false,
+      revealTwist: true, // Obligatoire
+    },
+    {
+      id: 'gladiateurs',
+      title: 'Les gladiateurs',
+      description: 'Le provocateur choisit un nouveau provocateur et une cible parmi les duellistes.',
+      emoji: '🗡️',
+      card: true,
+      revealTwist: true, // Obligatoire
+    }
   ];
+
+
 
   constructor() {}
 
-  getRandomTwist() {
+  getRandomTwist(id:string='') {
+    if (id !== '') {
+      return this.twists.find(twist => twist.id === id);
+    }
     const randomChance = Math.random(); // Renvoie un nombre entre 0 et 1
 
     // Si randomChance est inférieur ou égal à 0.5, il n'y a pas de twist
